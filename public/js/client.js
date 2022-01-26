edcb94ae7c1388a177285585102181c380e8c5fd
nir.vlad.zu <nir.vlad.zu@gmail.com>
2022-01-26 18:12:09 +0200

    Init commit

---
+++ public/js/client.js
@@ -0,0 +1,23 @@
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const messageOne = document.getElementById('message-one')
+const messageTwo = document.getElementById('message-two')
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const location = search.value
+    messageOne.textContent = 'Loading...'
+    messageTwo.textContent = ''
+
+    fetch(`http://localhost:3000/weather?address=${location}`).then((res) => {
+        res.json().then((data) => {
+            if (data.error) {
+                return (messageOne.textContent = data.error)
+            }
+            messageOne.textContent = data.location
+            messageTwo.textContent = data.forecast
+        })
+    })
+    search.value = ''
+})
